1f3962dc86268f3b5bd429cdfb310cf1a8276e44
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-08-25 23:12:44 +0900

    会員登録画面・機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
-  def new;end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
