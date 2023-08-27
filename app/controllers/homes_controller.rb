7a80fc5c513fa5ebedde2318b03e928241089690
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-08-27 19:42:54 +0900

    タイムライン作成・フッター作成

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,19 @@
+class HomesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
