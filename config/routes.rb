a274a5a82b131402948dc0f4f1706c73db37564b
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-11-06 21:13:36 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  root "tops#index"
+  root "top#index"
   get :sign_up, to: 'users#new'
   post :sign_up, to: 'users#create'
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   delete 'logout' => 'user_sessions#destroy', :as => :logout
-  get 'home', to: 'homes#show'
+  resource :home, only: %i[show]
   resource :users, only: %i[new create]
   resources :schedules
-  resources :profiles
+  resource :profiles
   resource :goal, only: %i[new create edit update destroy] do
     collection do
       post :skip
