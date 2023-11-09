7be5c62ef3e372b792f0ac12ccc22dd0f940c04c
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-11-09 22:06:57 +0900

    マイページ/edit 作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
+      redirect_to profiles_path, success: t('defaults.message.updated', item: User.model_name.human)
     else
       flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
       render :edit
