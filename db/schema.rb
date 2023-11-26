fc87b1f5402c069b5a6a705486e9b99bae7fe162
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-11-27 00:49:17 +0900

    Fix DBtable/achievements

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_30_073933) do
   enable_extension "plpgsql"
 
   create_table "achievements", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.bigint "step_id", null: false
-    t.boolean "completed"
-    t.integer "day_of_week"
-    t.integer "week_number"
     t.integer "times_completed"
     t.date "completed_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["step_id"], name: "index_achievements_on_step_id"
+    t.index ["user_id"], name: "index_achievements_on_user_id"
   end
 
   create_table "goals", force: :cascade do |t|
@@ -80,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_30_073933) do
   end
 
   add_foreign_key "achievements", "steps"
+  add_foreign_key "achievements", "users"
   add_foreign_key "goals", "users"
   add_foreign_key "schedules", "users"
   add_foreign_key "steps", "goals"
