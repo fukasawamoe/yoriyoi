e832a66ac22f1e36f0ef317ed6d8d4e9ce7d27cc
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-12-10 00:24:05 +0900

    Fix Cloudinaryのapiキーの参照先変更2

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,4 +3,5 @@ Cloudinary.config do |config|
   config.api_key = ENV['CLOUDINARY_API_KEY']
   config.api_secret = ENV['CLOUDINARY_API_SECRET']
   config.secure = true
+  config.cdn_subdomain = true
 end
