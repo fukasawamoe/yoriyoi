1a6cee5075cdd88a32bc5baf2d57da9b0aeb57ca
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-12-09 19:23:16 +0900

    Fix home_controller微リファクタ, form.jsをvar→letに、turbo:load追加

--- app/javascript/custom/form.js
+++ app/javascript/custom/form.js
@@ -1,41 +1,42 @@
-var selectAll = document.getElementById('select-all');
-var deselectAll = document.getElementById('deselect-all');
-var sortTasksButton = document.getElementById('sort-tasks-button');
+document.addEventListener('turbo:load', function() {
+  let selectAll = document.getElementById('select-all');
+  let deselectAll = document.getElementById('deselect-all');
+  let sortTasksButton = document.getElementById('sort-tasks-button');
 
-if (selectAll) {
-  selectAll.addEventListener('click', function() {
-    var checkboxes = document.querySelectorAll('input[type=checkbox]');
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].id != 'schedule_tasks_attributes_0_goal_select') {
-        checkboxes[i].checked = true;
+  if (selectAll) {
+    selectAll.addEventListener('click', function() {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].id != 'schedule_tasks_attributes_0_goal_select') {
+          checkboxes[i].checked = true;
+        }
       }
-    }
-  });
-}
+    });
+  }
 
-if (deselectAll) {
-  deselectAll.addEventListener('click', function() {
-    var checkboxes = document.querySelectorAll('input[type=checkbox]');
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].id != 'schedule_tasks_attributes_0_goal_select') {
-        checkboxes[i].checked = false;
+  if (deselectAll) {
+    deselectAll.addEventListener('click', function() {
+      let checkboxes = document.querySelectorAll('input[type=checkbox]');
+      for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].id != 'schedule_tasks_attributes_0_goal_select') {
+          checkboxes[i].checked = false;
+        }
       }
-    }
-  });
-}
-
-if (sortTasksButton) {
-  sortTasksButton.addEventListener('click', function() {
-    var sortedTasks = Array.from(document.querySelectorAll('.nested-fields')).sort(function(a, b) {
-      var timeA = a.querySelector('select[name$="[task_time(4i)]"]').value + ':' + a.querySelector('select[name$="[task_time(5i)]"]').value;
-      var timeB = b.querySelector('select[name$="[task_time(4i)]"]').value + ':' + b.querySelector('select[name$="[task_time(5i)]"]').value;
-      return timeA > timeB ? 1 : -1;
     });
+  }
 
-    var tasksContainer = document.getElementById('tasks-container');
-    sortedTasks.forEach(function(taskFields) {
-      tasksContainer.appendChild(taskFields);
+  if (sortTasksButton) {
+    sortTasksButton.addEventListener('click', function() {
+      let sortedTasks = Array.from(document.querySelectorAll('.nested-fields')).sort(function(a, b) {
+        let timeA = a.querySelector('select[name$="[task_time(4i)]"]').value + ':' + a.querySelector('select[name$="[task_time(5i)]"]').value;
+        let timeB = b.querySelector('select[name$="[task_time(4i)]"]').value + ':' + b.querySelector('select[name$="[task_time(5i)]"]').value;
+        return timeA > timeB ? 1 : -1;
+      });
+
+      let tasksContainer = document.getElementById('tasks-container');
+      sortedTasks.forEach(function(taskFields) {
+        tasksContainer.appendChild(taskFields);
+      });
     });
-  });
-}
-
+  }
+});
