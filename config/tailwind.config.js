6bc1986f750a86efa5ffcff2da3a9f4624946b77
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-12-10 23:52:01 +0900

    Fix ヘッダーDaisyUIに変更

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -25,20 +25,13 @@ module.exports = {
     require('daisyui'),   //ここを追加で導入終了。（,に気をつけないと悲しみを背負う）
   ],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#8cf75d",
-          "secondary": "#f3f4f6",
-          "accent": "#8bd0e5",
-          "neutral": "#202d3c",
-          "base-100": "#ffffff",
-          "info": "#81afda",
-          "success": "#36d399",
-          "warning": "#fca145",
-          "error": "#eb1e2b",
-        },
-      },
-    ],
+    themes: ["pastel","cupcake"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
   },
 }
