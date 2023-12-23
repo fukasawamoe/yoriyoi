c5d43af62588f0cffe8a0911f8caa7783c0b0f89
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2023-12-23 12:40:26 +0900

    Add ローディングアニメーションjs変更

--- app/javascript/custom/waiting.js
+++ app/javascript/custom/waiting.js
@@ -1,12 +1,7 @@
-window.onload = function() {
-  setTimeout(function() {
-    let loading = document.getElementById("showloading");
-    let contents = document.getElementById("contents");
-    
-    // ローディングのdivを非表示に変更
-    if (loading) loading.style.display = "none";
+document.addEventListener('turbo:load', function() {
+  document.getElementById('loading').style.display = 'none';
+});
 
-    // メインコンテンツのdivがある場合は表示に変更
-    if (contents) contents.style.display = "block";
-  }, 2000);
-};
+document.addEventListener('turbo:before-visit', function() {
+  document.getElementById('loading').style.display = 'block';
+});
