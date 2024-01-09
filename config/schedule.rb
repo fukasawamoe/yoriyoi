50408e4035a74a1ded8737617015f04527de43b1
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2024-01-09 22:53:19 +0900

    Update cron設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,21 +5,13 @@
 
 # Example:
 
-# Rails.rootを使用するために必要
-require File.expand_path(File.dirname(__FILE__) + '/environment')
+require File.expand_path(File.dirname(__FILE__) + "/environment") # Rails.root(Railsメソッド)を使用するために必要
+rails_env = ENV['RAILS_ENV'] || :development # cronを実行する環境変数(:development, :product, :test)
+set :environment, rails_env # cronを実行する環境変数をセット
+set :output, "#{Rails.root}/log/crontab.log" # cronのログ出力用ファイル
 
-# cronを実行する環境変数
-rails_env = ENV['RAILS_ENV'] || :development
-
-# cronを実行する環境変数をセット
-set :environment, rails_env
-
-# cronのログの吐き出し場所
-set :output, "#{Rails.root}/log/cron.log"
-
-#定期実行したい処理を記入
-every :day, at: '0:00 am' do
-  rake 'create_achievements_data:create_achievement_dayly_data'
+every 1.day, at: '0:00 am' do # タスクの実行間隔
+  rake "create_achievements_data:create_achievement_daily_data" # ← rake "タスクのファイル名 : タスク名"
 end
 
 # Learn more: http://github.com/javan/whenever
