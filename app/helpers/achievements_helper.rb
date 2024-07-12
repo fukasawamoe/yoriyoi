a4713f003a0a26f6d18a243b33c42de8796678ea
fukasawamoe <nekomeshi_nyan@yahoo.co.jp>
2024-07-12 21:10:24 +0900

    fix achievementグラフ　モーダル位置を非同期の際に動かないように調整

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -1,2 +1,11 @@
 module AchievementsHelper
+  def achievement_period
+    "#{Date.today - 6.days}から#{Date.today}まで"
+  end
+
+  def calculate_achievement_percentage(step)
+    true_count = step.achievements.where(daily: Date.today - 6.days..Date.today, check: true).count
+    total_count = step.achievements.where(daily: Date.today - 6.days..Date.today).count
+    total_count > 0 ? (true_count.to_f / total_count * 100).round : 0
+  end
 end
